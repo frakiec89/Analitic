360615aa1c472415005ac94d5c438f734e1e8e9c
РИШАТ Ахтямов <rishat-rus@mail.ru>
2020-11-16 15:30:08 +0400

    Добавьте файлы проекта.

---
+++ ConsoleApp11/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Analitic analitic = new Analitic();
+
+
+           var s =  analitic.GetDateTimes(new List<DateTime>() 
+            {
+                DateTime.Now.AddDays(2) , DateTime.Now , DateTime.Now.AddHours(1)
+            });
+
+
+            foreach (var item in s)
+            {
+                Console.WriteLine(item.Date);
+            }
+
+
+            Console.ReadLine();
+        }
+    }
+
+    public  class  Analitic
+    {
+        public  List<DateTime> GetDateTimes (List<DateTime>  dates)
+        {
+            List<DateTime> dateTimes = new List<DateTime>();
+
+
+            foreach (var item in dates)
+            {
+                var s = item.GetDateTimeFormats();
+                var d = DateTime.Parse(s[0]);
+
+                dateTimes.Add(d);
+            }
+
+            var data = from  d  in dateTimes
+                       group d by d.Date into g
+                       select new { Data = g.Key.Date , Count = g.Count()  };
+
+            data = data.OrderBy(x => x.Count).ThenBy(x=>x.Data).ToList();
+
+            dateTimes.Clear();
+
+            foreach (var item in data)
+            {
+                dateTimes.Add(item.Data);
+            }
+            return dateTimes;
+        }
+    }
+
+}
---
+++ ConsoleApp11Tests/AnaliticTests.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ConsoleApp11;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp11.Tests
+{
+    [TestClass()]
+    public class AnaliticTests
+    {
+
+        Analitic analitic = new Analitic();
+
+        [TestMethod()]
+        public void GetDateTimesTest()
+        {
+
+            var data = new List<DateTime> 
+            { 
+                DateTime.Parse("11.12.2020") ,
+                DateTime.Parse("12.12.2020") ,
+                DateTime.Parse("12.12.2020") ,
+                DateTime.Parse("13.12.2020") ,
+                DateTime.Parse("13.12.2020") ,
+                DateTime.Parse("13.12.2020") ,
+                DateTime.Parse("13.12.2020") ,
+            };
+
+
+            var expected = new List<DateTime>() { DateTime.Parse("11.12.2020") ,
+              DateTime.Parse("12.12.2020"), DateTime.Parse("13.12.2020")
+            };
+
+            var actual  = analitic.GetDateTimes(data);
+
+            Assert.AreEqual(expected.Count, actual.Count);
+
+            for ( int  i = 0; i<expected.Count; i++)
+            {
+                Assert.AreEqual(expected[i], actual[i]);
+            }
+        }
+
+        [TestMethod()]
+        public void GetDateTimesTest1()
+        {
+
+            var data = new List<DateTime>
+            {
+                DateTime.Parse("12.12.2020") ,
+                DateTime.Parse("12.12.2020") ,
+                DateTime.Parse("12.12.2020") ,
+                DateTime.Parse("13.12.2020") ,
+                DateTime.Parse("11.12.2020") ,
+            };
+
+
+            var expected = new List<DateTime>()
+            {   
+                DateTime.Parse("11.12.2020") ,
+                DateTime.Parse("13.12.2020"),
+                DateTime.Parse("12.12.2020"),
+            };
+
+            var actual = analitic.GetDateTimes(data);
+
+            Assert.AreEqual(expected.Count, actual.Count);
+
+            for (int i = 0; i < expected.Count; i++)
+            {
+                Assert.AreEqual(expected[i], actual[i]);
+            }
+        }
+    }
+}
